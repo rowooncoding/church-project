40704cf0ce7a9e976670b66b899d713a5e5c95b0
Rowoon <dev.rowoon@gmail.com>
2025-01-07 16:51:21 +0900

    feat: 헤더 상단 로고 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 function Header() {
     return (
-        <div>hello i am header!!</div>
+        <div className='flex items-start justify-center h-screen'>
+            <Image 
+                src="/selfmade-ahabalogo.png"
+                    alt="Not fount Image"
+                width={1200}
+                height={400}
+            />
+        </div>
+            
     )
 }
 
