d52437e07e38600dcbc3a445f5d11808bda126bc
Rowoon <dev.rowoon@gmail.com>
2025-01-14 13:46:28 +0900

    init: firebase 초기화 방식 수정

--- firebase/auth.js
+++
@@ -1,7 +0,0 @@
-import firebaseApp from './firebaseApp'
-import { getAuth } from 'firebase/auth'
-
-const auth = getAuth( firebase );
-
-export default auth;
-
--- firebase/db.js
+++
@@ -1,6 +0,0 @@
-import firebaseApp from './firebaseApp'
-import { getFirestore } from 'firebase/firestore'
-
-const db = getFirestore( firebaseApp );
-
-export default db;
